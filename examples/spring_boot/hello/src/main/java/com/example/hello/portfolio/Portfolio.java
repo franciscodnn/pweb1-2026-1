211d36b7ebcad31ab035472fdcc535a31c8bdc54
Francisco Dantas <dantas.nobre@ifpb.edu.br>
2026-06-10 14:37:53 -0300

    feat: add lombok package

--- examples/spring_boot/hello/src/main/java/com/example/hello/portfolio/Portfolio.java
+++ examples/spring_boot/hello/src/main/java/com/example/hello/portfolio/Portfolio.java
@@ -4,9 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-// @Data
-// @AllArgsConstructor
-// @NoArgsConstructor
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Portfolio {
     private Long id;
     private String title;
@@ -14,44 +14,4 @@ public class Portfolio {
     private String repo;
     private String[] techs;
 
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    public String getRepo() {
-        return repo;
-    }
-    public void setRepo(String repo) {
-        this.repo = repo;
-    }
-    public String[] getTechs() {
-        return techs;
-    }
-    public void setTechs(String[] techs) {
-        this.techs = techs;
-    }
-
-    public Portfolio () { }
-
-    public Portfolio(Long id, String title, String description, String repo, String[] techs) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.repo = repo;
-        this.techs = techs;
-    }
 }
